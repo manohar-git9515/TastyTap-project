34e2794c2f958e88a7ba35f364f674598647d9a4
manohar-git9515 <manoharbarath9515@gmail.com>
2025-07-29 18:03:44 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,7 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const placeOrder = async (req, res) => {
 
-    const frontend_url = "http://localhost:5174";
+    const frontend_url = "https://tastytap-frontend-1dcx.onrender.com";
 
     try {
         // Validate required fields
